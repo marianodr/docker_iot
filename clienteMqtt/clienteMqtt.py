08213056772f3776c82842ed4330032263294a0e
AngeSilk <angesilk007@gmail.com>
2024-05-01 03:42:15 -0300

    clientemqtt exited with code 0

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -3,11 +3,23 @@ import ssl
 import os
 import logging
 import aiomqtt
+import certifi
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s: %(message)s %(task_name)s',
                     level=logging.INFO,
                     datefmt='%d/%m/%Y %H:%M:%S %z')
-    
+
+#Definir las corrutinas para procesar los mensajes de los tópicos
+async def handle_topic_1(client):
+    async for message in client.messages:
+        if message.topic == topic_subscribe_1:
+            await process_topic_1(message)
+
+async def handle_topic_2(client):
+    async for message in client.messages:
+        if message.topic == topic_subscribe_2:
+            await process_topic_2(message)
+
 async def process_topic_1(message):
     logging.info(f'Received message in topic 1: {message.payload}')
 
@@ -30,23 +42,24 @@ async def counter_incrementer():
         logging.info(f'Counter value: {counter}')
 
 async def main():
+    logging.info("hola")
     # Lee la variable de entorno para MQTT_SERVER
-    server_address = os.environ.get('MQTT_SERVER')
-    if not server_address:
-        logging.error('MQTT_SERVER environment variable not set')
-        return
+    server_address = os.environ['MQTT_SERVER']
+    #if not server_address:
+    #    logging.error('MQTT_SERVER environment variable not set')
+    #    return
     
     # Lee las variables de entorno para los tópicos
-    topic_subscribe_1 = os.environ.get('TOPIC_SUBSCRIBE_1')
-    topic_subscribe_2 = os.environ.get('TOPIC_SUBSCRIBE_2')
-    topic_publish = os.environ.get('TOPIC_PUBLISH')
+    topic_subscribe_1 = os.environ['TOPIC_SUBSCRIBE_1']
+    topic_subscribe_2 = os.environ['TOPIC_SUBSCRIBE_2']
+    topic_publish = os.environ['TOPIC_PUBLISH']
     
-    if not topic_subscribe_1 or not topic_subscribe_2 or not topic_publish:
-        logging.error('One or more required environment variables are not set')
-        return
+    #if not topic_subscribe_1 or not topic_subscribe_2 or not topic_publish:
+    #    logging.error('One or more required environment variables are not set')
+    #    return
     
     # Crea la conexión tls
-    tls_context = ssl.create_default_context()
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
@@ -61,30 +74,20 @@ async def main():
         await client.subscribe(topic_subscribe_1)
         await client.subscribe(topic_subscribe_2)
 
-        async with asyncio.TaskGroup() as tg:
-            # Definir las corrutinas para procesar los mensajes de los tópicos
-            async def handle_topic_1():
-                async for message in client.messages:
-                    if message.topic == topic_subscribe_1:
-                        await process_topic_1(message)
-            
-            async def handle_topic_2():
-                async for message in client.messages:
-                    if message.topic == topic_subscribe_2:
-                        await process_topic_2(message)
-            
+        async with asyncio.TaskGroup() as tg:            
             # Crear corrutinas específicas para manejar cada tópico
-            tg.create_task(handle_topic_1())
-            tg.create_task(handle_topic_2())
+            tg.create_task(handle_topic_1(client))
+            tg.create_task(handle_topic_2(client))
 
             # Corrutina para publicar el estado del contador
-            tg.create_task(counter_publisher(topic_publish, client), name='counter_publisher')
+            #tg.create_task(counter_publisher(topic_publish, client), name='counter_publisher')
 
             # Corrutina para incrementar el contador
-            tg.create_task(counter_incrementer(), name='counter_incrementer')
+            #tg.create_task(counter_incrementer(), name='counter_incrementer')
 
 if __name__ == "main":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        logging.info('Received keyboard interrupt, exiting...')
+    asyncio.run(main())
+    #try:
+    #    asyncio.run(main())
+    #except KeyboardInterrupt:
+    #    logging.info('Received keyboard interrupt, exiting...')

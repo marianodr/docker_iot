56b26e006343a3b5c008ce5503cbc84bf1739b3c
mdr <marianodrodriguez03@gmail.com>
2024-06-06 17:03:45 -0300

    Modificaciones en layout y main.js para el cambio de tema

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -10,3 +10,30 @@ if(btnDelete) {
     });
   })
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const themeSelector = document.getElementById('themeSelector');
+  const themeStylesheet = document.getElementById('themeStylesheet');
+
+  // Verificar el tema guardado en Local Storage
+  const savedTheme = localStorage.getItem('theme') || 'light';
+  document.body.classList.add(savedTheme);
+  themeStylesheet.href = savedTheme === 'dark' 
+      ? 'https://bootswatch.com/5/darkly/bootstrap.min.css' 
+      : 'https://bootswatch.com/5/cosmo/bootstrap.min.css';
+  themeSelector.value = savedTheme;
+
+  // Cambiar el tema cuando el selector cambia
+  themeSelector.addEventListener('change', function () {
+      const selectedTheme = themeSelector.value;
+      document.body.classList.toggle('dark', selectedTheme === 'dark');
+      document.body.classList.toggle('light', selectedTheme === 'light');
+
+      themeStylesheet.href = selectedTheme === 'dark' 
+          ? 'https://bootswatch.com/5/darkly/bootstrap.min.css' 
+          : 'https://bootswatch.com/5/cosmo/bootstrap.min.css';
+
+      // Guardar el tema seleccionado en Local Storage
+      localStorage.setItem('theme', selectedTheme);
+  });
+});
